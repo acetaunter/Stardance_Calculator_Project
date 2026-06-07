0cd0cff146d46d9b331fa4655420d0bee0db7d17
Blace_magician <mcalisterlake@gmail.com>
2026-06-07 12:12:01 -0700

    Commit was for basic javafx setups and for a basic applications for button

--- src/Main/Java/Stardance_Calculator/Main.java
+++ src/Main/Java/Stardance_Calculator/Main.java
@@ -1,57 +1,137 @@
 package Stardance_Calculator;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
-
+import java.util.ArrayList;
+import java.util.Collections;
 
 
 public class Main extends Application{
-
+    // 25 pixel incremental for x direction
+    int y_pos = 0;
+    int x_pos = 130;
+    int counter = 0;
     @Override
     public void start(Stage stage){
         stage.setScene(Calculator_UI(stage));
     }
     public Scene Calculator_UI(Stage stage){
-        GridPane root = new GridPane();
-        Button one = new Button("1");
-        root.add(one,1,0,4,4);
-
-        Button two = new Button("2");
-        root.add(two,2,0,4,4);
-
-        Button three = new Button("3");
-        root.add(three,1,0,4,4);
-
-        Button four = new Button("4");
-        root.add(four,1,0,3,3);
-
-        Button five = new Button("5");
-        root.add(five,2,0,3,3);
-
-        Button six = new Button("6");
-        root.add(six,3,0,3,3);
-
-        Button seven = new Button("7");
-        root.add(seven,1,0,2,2);
-
-        Button eight = new Button("8");
-        root.add(eight,2,0,2,1);
-
-        Button nine = new Button("9");
-        root.add(nine,3,0,2,2);
-
-        Button zero = new Button("0");
-        root.add(four,1,0,5,5);
-        //root.add(zero,4,0,2,2);
+        Pane root = new Pane();
         TextField User_equation= new TextField();
+       User_equation.setLayoutX(130);
+       User_equation.setLayoutY(0);
         root.getChildren().add(User_equation);
 
-        Scene scene = new Scene(root,700,700);
+        Button one = new Button("1");
+        one.setLayoutX(x_pos +10);
+        one.setLayoutY(y_pos +75);
+        root.getChildren().add(one);
+        one.setOnAction(e->{
+            User_equation.appendText("1");
+            counter++;
+        });
+
+        Button two = new Button("2");
+        two.setLayoutX(x_pos +30);
+        two.setLayoutY(y_pos +75);
+        root.getChildren().add(two);
+        two.setOnAction(e->{
+            User_equation.appendText("2");
+            counter++;
+        });
+
+
+        Button three = new Button("3");
+        three.setLayoutX(x_pos +50);
+        three.setLayoutY(y_pos +75);
+       root.getChildren().add(three);
+        three.setOnAction(e->{
+            User_equation.appendText("3");
+            counter++;
+        });
+
+        Button four = new Button("4");
+        four.setLayoutX(x_pos+10);
+        four.setLayoutY(y_pos +50);
+        root.getChildren().add(four);
+        four.setOnAction(e->{
+            User_equation.appendText("4");
+            counter++;
+        });
+
+
+        Button five = new Button("5");
+        five.setLayoutX(x_pos +30);
+        five.setLayoutY(y_pos +50);
+        root.getChildren().add(five);
+        five.setOnAction(e->{
+            User_equation.appendText("5");
+            counter++;
+        });
+
+
+        Button six = new Button("6");
+        six.setLayoutX(x_pos +50);
+        six.setLayoutY(y_pos +50);
+        root.getChildren().add(six);
+        six.setOnAction(e->{
+            User_equation.appendText("6");
+            counter++;
+        });
+
+        Button seven = new Button("7");
+        seven.setLayoutX(x_pos +10);
+        seven.setLayoutY(y_pos +25);
+        root.getChildren().add(seven);
+        seven.setOnAction(e->{
+            User_equation.appendText("7");
+            counter++;
+        });
+
+        Button eight = new Button("8");
+        eight.setLayoutX(x_pos +30);
+        eight.setLayoutY(y_pos +25);
+        root.getChildren().add(eight);
+        eight.setOnAction(e->{
+            User_equation.appendText("8");
+            counter++;
+        });
+
+        Button nine = new Button("9");
+        nine.setLayoutX(x_pos +50);
+        nine.setLayoutY(y_pos +25);
+        root.getChildren().add(nine);
+        nine.setOnAction(e->{
+            User_equation.appendText("9");
+            counter++;
+        });
+
+        Button zero = new Button("0");
+        zero.setLayoutX(x_pos +10);
+        zero.setLayoutY(y_pos +100);
+        root.getChildren().add(zero);
+        zero.setOnAction(e->{
+            User_equation.appendText("0");
+            counter++;
+        });
+
+        //operations code
+
+        Button equals = new Button("=");
+        root.getChildren().add(equals);
+        equals.setOnAction(e->{
+            ArrayList<String> equation = new ArrayList<String>(Collections.singleton(User_equation.getText()));
+            System.out.print(equation);
+        });
+
+        Scene scene = new Scene(root,400,400);
 
         stage.setScene(scene);
         stage.show();
