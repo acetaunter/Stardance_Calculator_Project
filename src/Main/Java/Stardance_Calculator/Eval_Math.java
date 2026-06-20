580cc5c6d79f3416e9258c0ee1bcdec200c768de
Blace_magician <mcalisterlake@gmail.com>
2026-06-20 16:17:36 -0700

    it actually like prints something out on the text input

--- src/Main/Java/Stardance_Calculator/Eval_Math.java
+++ src/Main/Java/Stardance_Calculator/Eval_Math.java
@@ -9,6 +9,7 @@ public class Eval_Math {
 
     public static String Equation(String i_equation){
         ArrayList<Character> c_equation = new ArrayList<>(i_equation.length());
+        StringBuilder ceq = new StringBuilder( String.valueOf(c_equation));
         //creates the entire equation
         for(int e = 0; e < i_equation.length(); e++){
         c_equation.add(e, i_equation.charAt(e));
@@ -222,5 +223,64 @@ public class Eval_Math {
                 c_equation.add(l_index+i,(answer.charAt(i)));
             }
 
-    }}}
+    }
+    while(c_equation.contains('+') || c_equation.contains('-')){
+        char operator = ' ';
+        double l_number;
+        double  r_number;
+        double sum_number = 0;
+        int operator_index = -1;
+        for(int g =0; g<c_equation.size(); g++ ){
+            char r = c_equation.get(g);
+            if(r == '-' || r == '+'){
+                operator = r;
+                operator_index =g;
+                break;
+            }
+        }
+
+        //parser logic
+        int l_index = operator_index-1;
+        StringBuilder sl_index = new StringBuilder();
+        sl_index.append(c_equation.get(l_index));
+        while(  l_index -1>=0 && (Character.isDigit(c_equation.get(l_index-1)))) {
+            l_index--;
+            sl_index.append(c_equation.get(l_index));
+        }
+        sl_index.reverse();
+        l_number = Double.parseDouble(String.valueOf(sl_index));
+
+        // important note: r_index has to stop at the last number for your logic
+        int r_index = operator_index+1;
+        StringBuilder rl_index = new StringBuilder();
+        rl_index.append(c_equation.get(r_index));
+        while(  r_index +1 <c_equation.size() && (Character.isDigit(c_equation.get(r_index+1)))){
+            r_index++;
+            rl_index.append(c_equation.get(r_index));
+        }
+        r_number = Double.parseDouble(String.valueOf(rl_index));
+
+        if(operator == '-'){
+            sum_number = l_number - r_number;
+        }
+        if(operator == '+'){
+            sum_number = l_number + r_number;
+        }
+
+        if (r_index >= l_index) {
+            c_equation.subList(l_index, r_index + 1).clear();
+        }
+        StringBuilder answer = new StringBuilder(String.valueOf(sum_number));
+        for(int i = 0; i < answer.length();i++){
+            c_equation.add(l_index+i,(answer.charAt(i)));
+        }
+
+    }
+    StringBuilder final_answer = new StringBuilder();
+        for(int i = 0; i< c_equation.size(); i++){
+            final_answer.append(c_equation.get(i));
+        }
+        return final_answer.toString();
+
+    }}
 
